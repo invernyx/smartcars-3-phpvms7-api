c8e1c62cf1ba67e3ed156aff8a194943f12aa59d
Taylor Broad <taylorbroad@gmail.com>
2024-05-10 22:28:33 +0900

    change: cron nightly to hourly

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\SmartCARS3phpVMS7Api\Providers;
 
+use App\Events\CronHourly;
 use App\Events\CronNightly;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Modules\SmartCARS3phpVMS7Api\Listeners\DeleteCharterFlights;
@@ -12,7 +13,7 @@ class EventServiceProvider extends ServiceProvider
      * The event listener mappings for the application.
      */
     protected $listen = [
-        CronNightly::class => [DeleteCharterFlights::class]
+        CronHourly::class => [DeleteCharterFlights::class]
     ];
 
     /**
